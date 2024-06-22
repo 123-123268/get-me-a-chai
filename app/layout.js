0bce424c6e6b9086bb419ed3629f698907043633
123-123268 <kavyayadav1980@gmail.com>
2024-06-22 11:41:18 +0530

    signin not working

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,8 +12,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en">     
+      <body className={inter.className}> 
+       <SessionWrapper>     
+      <Navbar/>
+      <div className="min-h-[79.5vh] bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]">
+        {children}
+      </div>     
+      <Footer/>
+      </SessionWrapper> 
+      </body>
     </html>
   );
 }
---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div  className='flex justify-center bg-blue-950 text-white px-3 items-center h-16'>
+      <p>Copyright &copy; GetMeA_Chai - Fund your Projects with chai!</p>
+    </div>
+  )
+}
+
+export default Footer
